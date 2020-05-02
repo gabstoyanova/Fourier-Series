3ffe968ee7a83b29ef9614565aa1024d2bff4a79
Gabriela <gaby9898@gmail.com>
2020-05-02 20:07:03 +0300

    css and user interaction

--- fourier.js
+++ fourier.js
@@ -2,7 +2,7 @@ let canvas = document.querySelector("canvas");
 let ctx = canvas.getContext("2d");
 
 const P = 2 * Math.PI;
-const SCALE = 70;
+const AMPLITUDE = 90;
 let time = 0;
 let x;
 let y;
@@ -12,9 +12,8 @@ let valuePointer = 0;
 
 function fourier(n) {
     let phase = n * time * P;
-    let radius = 4 / (n * Math.PI) * SCALE;
+    let radius = 4 / (n * Math.PI) * AMPLITUDE;
 
-    ctx.lineWidth = 1.5;
     ctx.strokeStyle = "white";
     ctx.beginPath();
     ctx.arc(x, y, radius, 0, P);
@@ -28,7 +27,7 @@ function fourier(n) {
 }
 
 function drawTrajectory() {
-    ctx.strokeStyle = "red"
+    ctx.strokeStyle = "#f75"
 
     if (trajectoryValues.length == 0) {
         return;
@@ -45,47 +44,54 @@ function drawTrajectory() {
 function translateLine() {
     ctx.beginPath();
     ctx.moveTo(x, y);
-    ctx.lineTo(450, y);
-    ctx.strokeStyle = "red";
+    ctx.lineTo(500, y);
+    ctx.strokeStyle = "#f75";
     ctx.stroke();
 }
 
 function drawWave() {
-
     ctx.beginPath();
-    let x = 450;
+    let x = 500;
 
     waveValues[valuePointer] = y;
     valuePointer++;
     ctx.strokeStyle = "white";
 
-    ctx.moveTo(x + 5, y + 0.5);
-    for (var i = 1; i < 300; ++i) {
-            ctx.lineTo(x + i , waveValues[(valuePointer - i)]);
+    ctx.moveTo(x + 4, y + 0.5);
+    for (var i = 1; i < 450; ++i) {
+        ctx.lineTo(x + i, waveValues[(valuePointer - i)]);
     }
     ctx.stroke();
 }
 
-function animate() {
-    ctx.fillRect(0, 0, canvas.width, canvas.height)
+function setOutputValue() {
+    var output = document.getElementById('slider').value;
+    document.getElementById('output').innerHTML = parseInt(output) + 1;
+}
 
-    x = 200;
-    y = 200;
-    let phasors = 4;
-    for (let n = 0; n <= phasors; n++) {
+function animate() {
+    canvas.width = canvas.clientWidth;
+    canvas.height = canvas.clientHeight;
+    ctx.lineWidth = 2;
+
+    x = 250;
+    y = 250;
+    let epicycles = document.getElementById("slider").value;
+    setOutputValue()
+    for (let n = 0; n <= parseInt(epicycles); n++) {
         fourier(2 * n + 1);
     }
 
     //endpoint
-    ctx.strokeStyle = 'red';
+    ctx.strokeStyle = '#f75';
     ctx.strokeRect(x, y, 3, 3);
 
     //trajectory
     trajectoryValues.push( {x1: x, y1: y, x2: x-1, y2: y-1} );
     drawTrajectory();
+
     translateLine();
     drawWave();
-    
     time += 0.01
     window.requestAnimationFrame(animate);
 }
